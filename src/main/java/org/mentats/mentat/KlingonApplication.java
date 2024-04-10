bdf1b4753947aef60b612d7d17811e0f216b5403
Joshua Summers <jsummers@JOSHUAs-MacBook-Pro.local>
2024-04-09 23:20:55 -0700

    Initial scaffolding for Spring Boot Framework

---
+++ src/main/java/org/mentats/mentat/KlingonApplication.java
@@ -0,0 +1,13 @@
+package org.mentats.mentat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlingonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlingonApplication.class, args);
+	}
+
+}
