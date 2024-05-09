71b1def5366a4027d0c13240b082aa31e4665e5e
Telmen17 <telmen7948@gmail.com>
2024-05-09 16:35:50 -0700

    This is the Course.java initial code.

---
+++ src/main/java/Course.java
@@ -0,0 +1,11 @@
+/** This class stores data of any course an instructor wants to add i.e. Professor Black adds MATH260; should try to
+ * have logic that shows all students registered for the class (may have to extend another class not build yet; TBD)
+ * @author Telmen Enkhtuvshin
+ */
+public class Course {
+    String courseID;
+    String courseName;
+    String instructorID;
+    String term;
+
+}
